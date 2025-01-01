1c88143cf14624c018145f561dee36b93641bfef
Cidree <adrian.cidre@gmail.com>
2025-01-01 12:49:44 +0100

    support for local files on db_write

--- R/db_write.R
+++ R/db_write.R
@@ -5,7 +5,7 @@
 #' The table is created in the specified schema of the DuckDB database.
 #'
 #' @param conn a connection object to a DuckDB database
-#' @param data a \code{sf} object to write to the DuckDB database
+#' @param data a \code{sf} object to write to the DuckDB database, or a local file
 #' @param name a character string of length one specifying the name of the table,
 #' or a character string of length two specifying the schema and table names.
 #' @param overwrite whether to overwrite the existing table if it exists
@@ -56,41 +56,49 @@ ddbs_write_vector <- function(conn, data, name, overwrite = FALSE) {
         schema_name <- name[1]
         query_name <- paste0(name, collapse = ".")
     } else {
-        table_name   <- name
+        table_name  <- name
         schema_name <- "main"
-        query_name <- name
+        query_name  <- name
     }
     if (table_name %in% DBI::dbListTables(conn) & !overwrite)
         cli::cli_abort("The provided name is already present in the database. Please, use `overwrite = TRUE` or choose a different name.")
 
-    # 2. Handle unsupported geometries
-    unsupported_types <- c("GEOMETRYCOLLECTION")
-    geom_types <- unique(sf::st_geometry_type(data))
-    if (any(geom_types %in% unsupported_types)) {
-        cli::cli_abort("Unsupported geometry types found: {paste(geom_types[geom_types %in% unsupported_types], collapse = ', ')}")
-    }
-
-    # 3. Handle overwrite
+    # 2. Handle overwrite
     if (overwrite) {
         DBI::dbExecute(conn, glue::glue("DROP TABLE IF EXISTS {query_name};"))
         cli::cli_alert_info("Table <{query_name}> dropped")
     }
 
-    # 4. Prepare data for writing - import as data frame with geom as binary
-    ## Get geometry column name
-    geom_name <- setdiff(names(data), names(sf::st_drop_geometry(data)))
-    ## Extract geometry as binary and append to data frame
-    wkb_data <- sf::st_as_binary(sf::st_geometry(data), EWKB = TRUE)
-    data_df <- as.data.frame(data)
-    data_df[[geom_name]] <- wkb_data  # Ensure raw data is preserved
+    ## 3. insert data
+    if (inherits(data, "sf")) {
+        # 3. Handle unsupported geometries
+        unsupported_types <- c("GEOMETRYCOLLECTION")
+        geom_types <- unique(sf::st_geometry_type(data))
+        if (any(geom_types %in% unsupported_types)) {
+            cli::cli_abort("Unsupported geometry types found: {paste(geom_types[geom_types %in% unsupported_types], collapse = ', ')}")
+        }
 
-    ## Write data into DuckDB
-    DBI::dbWriteTable(conn, DBI::Id(schema = schema_name, table = table_name), data_df, overwrite = overwrite, field.types = c(geom_name = "BLOB"))
-    ## Convert to spatial
-    DBI::dbExecute(conn, glue::glue("
+        # 4. Prepare data for writing - import as data frame with geom as binary
+        ## Get geometry column name
+        geom_name <- setdiff(names(data), names(sf::st_drop_geometry(data)))
+        ## Extract geometry as binary and append to data frame
+        wkb_data <- sf::st_as_binary(sf::st_geometry(data), EWKB = TRUE)
+        data_df <- as.data.frame(data)
+        data_df[[geom_name]] <- wkb_data  # Ensure raw data is preserved
+
+        ## Write data into DuckDB
+        DBI::dbWriteTable(conn, DBI::Id(schema = schema_name, table = table_name), data_df, overwrite = overwrite, field.types = c(geom_name = "BLOB"))
+        ## Convert to spatial
+        DBI::dbExecute(conn, glue::glue("
         ALTER TABLE {query_name}
         ALTER COLUMN {geom_name} SET DATA TYPE GEOMETRY USING ST_GeomFromWKB({geom_name});
     "))
+    } else {
+        DBI::dbExecute(
+            conn,
+            glue::glue("CREATE TABLE {query_name} AS SELECT * FROM ST_Read('{data}')")
+        )
+    }
 
 
     # 5. User feedback
@@ -99,3 +107,5 @@ ddbs_write_vector <- function(conn, data, name, overwrite = FALSE) {
     return(invisible(TRUE))
 
 }
+
+
