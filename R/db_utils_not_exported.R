3b5035ba6c4dc7f07ae2714ef56ae51a752772a0
Cidree <adrian.cidre@gmail.com>
2025-04-17 10:39:56 +0200

    cran note: return value for non-exported funs

--- R/db_utils_not_exported.R
+++ R/db_utils_not_exported.R
@@ -1,11 +1,12 @@
 
 ## dbConnCheck
 
-##' Check if a supported DuckDB connection
-##'
-##' @param conn A DuckDB connection
-##'
-##' @keywords internal
+#' Check if a supported DuckDB connection
+#'
+#' @param conn A DuckDB connection
+#'
+#' @keywords internal
+#' @returns TRUE (invisibly) for successful import
 dbConnCheck <- function(conn) {
     if (inherits(conn, "duckdb_connection")) {
         return(invisible(TRUE))
@@ -14,13 +15,14 @@ dbConnCheck <- function(conn) {
     }
 }
 
-##' Get column names in a DuckDB database
-##'
-##' @param conn A DuckDB connection
-##' @param x name of the table
-##' @param rest whether to return geometry column name, of the rest of the columns
-##'
-##' @keywords internal
+#' Get column names in a DuckDB database
+#'
+#' @param conn A DuckDB connection
+#' @param x name of the table
+#' @param rest whether to return geometry column name, of the rest of the columns
+#'
+#' @keywords internal#'
+#' @returns name of the geometry column of a table
 get_geom_name <- function(conn, x, rest = FALSE) {
     info_tbl <- DBI::dbGetQuery(conn, glue::glue("PRAGMA table_info('{x}');"))
     if (rest) info_tbl[!info_tbl$type == "GEOMETRY", "name"] else info_tbl[info_tbl$type == "GEOMETRY", "name"]
