f66d77ccfb13f3acedde6f52018808d51f86f47d
rafapereirabr <rafa.pereira.br@gmail.com>
2025-12-20 22:27:26 -0300

    fix test

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -9,7 +9,7 @@ countries_sf <- subset(countries_sf, CNTR_ID %in% c("AR", "BR", "BO", "PE", "PY"
 argentina_sf <- sf::st_read(system.file("spatial/argentina.geojson", package = "duckspatial"))
 
 # read lines data
-rivers_sf <- st_read(system.file("spatial/rivers.geojson", package = "duckspatial"))
+rivers_sf <- sf::st_read(system.file("spatial/rivers.geojson", package = "duckspatial"))
 
 ## create points data
 set.seed(42)
